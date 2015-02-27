2d4e98775dd4b4ea8b2eb3145bcf0e667aeab0b5
Sambath Prum <psb.sambath@gmail.com>
2015-02-26 20:14:18 -0600

    add create action to users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,4 +14,41 @@ RSpec.describe UsersController, type: :controller do
 
     it { should respond_with 200 }
   end
+
+  describe 'GET #create' do
+    context 'when is successfully created' do
+      before :each do
+        @user_attributes = attributes_for :user
+        post :create, { user: @user_attributes }
+      end
+
+      it 'renders the json representation of the user record created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(@user_attributes[:email])
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context 'when is not created' do
+      before :each do
+        invalid_user_attributes = { password: 'helloworld',
+                                    password_confirmation: 'helloworld'
+                                   }
+        post :create, { user: invalid_user_attributes }
+      end
+
+      it 'renders an errors json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include("can't be blank")
+      end
+
+      it { respond_with 422 }
+    end
+  end
 end
