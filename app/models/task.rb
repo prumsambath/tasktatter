6feb3dc15bc3b297a585567907b90836e156ced0
Sambath Prum <psb.sambath@gmail.com>
2015-03-02 17:42:55 -0600

    add tests to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :list
+
+  validates :title, presence: true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+
+  it { should belong_to :list }
 end
