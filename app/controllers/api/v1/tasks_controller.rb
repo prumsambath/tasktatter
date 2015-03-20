26775dbf607ebf999bcc9de7e3190a8f284f8c9f
Sambath Prum <psb.sambath@gmail.com>
2015-03-19 19:09:42 -0500

    refactor

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,31 +2,34 @@ class Api::V1::TasksController < ApplicationController
   include Authenticable
 
   before_action :authenticate_with_token!
+  before_action :set_list, only: [:create, :update]
   respond_to :json
 
   def create
-    list = List.find(params[:list_id])
-    task = list.tasks.build(task_params)
+    task = @list.tasks.build(task_params)
 
-    if list.editable?(current_user) && list.save
-      render json: list.tasks, status: :created
+    if @list.editable?(current_user) && @list.save
+      render json: @list.tasks, status: :created
     else
       render json: task.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    list = List.find(params[:list_id])
     task = Task.find(params[:id])
 
-    if list.editable?(current_user) && task.update_attributes(task_params)
-      render json: list.tasks, status: :ok
+    if @list.editable?(current_user) && task.update_attributes(task_params)
+      render json: @list.tasks, status: :ok
     else
       render json: task.errors, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :completed)
